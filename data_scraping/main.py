ca280fc232d9da16a0cc1eabd51f448af609effe
Omri Fridental <omri.fridental@pagaya.com>
2020-06-05 17:47:27 +0300

    change in scraped subreddit list

--- data_scraping/main.py
+++ data_scraping/main.py
@@ -11,13 +11,20 @@ DATASET_DIR = pathlib.Path('data') / 'dataset'
 
 def collect_pictures(dataset_size):
 
-    memes_subreddits = ['memes']
-    non_memes_subreddits = ['pics', 'rarepuppers', 'carporn', 'cityporn', 'selfie', 'sports', 'cats', 'NatureIsFuckingLit', 'ps4', 'texts']
+    memes_subreddits = ['memes', 'dankmemes']
+    major_non_memes_subreddits = ['pics', 'Twitter', 'FacebookCringe', 'texts', 'cityporn', 'selfie', 'NatureIsFuckingLit']
+    minor_non_memes_subreddits = ['sports', 'cats', 'carporn', 'rarepuppers', 'ps4']
+
+    major_percentage = 0.7
+    minor_percentage = 1 - major_percentage
 
     for subreddit in memes_subreddits:
-        download(subreddit, dataset_size // (2 * len(memes_subreddits)))
-    for subreddit in non_memes_subreddits:
-        download(subreddit, dataset_size // (2 * len(non_memes_subreddits)))
+        download(subreddit, int(dataset_size * 0.5 / len(memes_subreddits)))
+
+    for subreddit in major_non_memes_subreddits:
+        download(subreddit, int(dataset_size * 0.5 * major_percentage / len(major_non_memes_subreddits)))
+    for subreddit in minor_non_memes_subreddits:
+        download(subreddit, int(dataset_size * 0.5 * minor_percentage / len(minor_non_memes_subreddits)))
 
     dataframes = []
     for subreddit in os.listdir(DATA_DIR):
@@ -31,16 +38,16 @@ def reorder_directories():
     # open dataset description csv:
     dataset_desc = pd.read_csv(DATA_CSV_PATH)
 
-    # seperate to train and test:
+    # reorder in cls_i/example1,...,example_n. also hash pictures name for uniformity.
     classes = ['Yes', 'No']
     for cls in classes:
         os.system(f'mkdir -p {DATASET_DIR / cls}')
         file_paths = dataset_desc[dataset_desc['Memes?'] == cls]['Image Path']
         for file_path in file_paths:
             _, img_type = os.path.splitext(file_path)
-            os.system(f'cp {file_path} {DATASET_DIR / cls / (str(hashlib.sha256(str(file_path).encode()).hexdigest()) + img_type)}')
+            os.system(f'cp {file_path} {DATASET_DIR / cls / (str(hashlib.sha1(str(file_path).encode()).hexdigest()) + img_type)}')
 
 
 
-collect_pictures(1000)
+collect_pictures(10000)
 reorder_directories()
