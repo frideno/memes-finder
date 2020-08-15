1272d7d9cfdf41f9d7be457bf3e1d02095a7c9bf
guyguy40 <46048002+guyguy40@users.noreply.github.com>
2020-08-15 12:30:50 +0300

    Update file to newer version

--- model/liteConverter.py
+++ model/liteConverter.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
+import sys
 
-loaded = tf.keras.models.load_model("currModel")
+loaded = tf.keras.models.load_model(sys.argv[1])
 converter = tf.lite.TFLiteConverter.from_keras_model(loaded)
 tflite_model = converter.convert()
-open("memeClassifierV4.tflite", "wb").write(tflite_model)
+open(sys.argv[2], "wb").write(tflite_model)
